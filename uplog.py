86e329d8114270671c85efb93e0d86be14a1fdc5
Alvaro Figueroa <alvaro@greencore.co.cr>
2020-01-14 18:28:14 -0600

    Adding initial upload.py code

---
+++ uplog.py
@@ -0,0 +1,24 @@
+import sys
+import time
+from scapy.all import IP, ICMP, sr1
+ip = sys.argv[1]
+icmp = IP(dst=ip)/ICMP()
+cur_min = 0
+f = open("uplog.txt","a")
+try:
+    while True:
+        resp = sr1(icmp,timeout=1)
+        secs = time.localtime()
+        if cur_min != secs.tm_min:
+            cur_min = secs.tm_min
+            f.write("]\n["+str(secs.tm_hour)+":"+str(secs.tm_min)+":"+str(f"{secs.tm_sec:02d}")+"]|[")
+    
+        if resp == None:
+            f.write("-")
+        else:
+            f.write(".")
+        time.sleep(1)
+        f.flush()
+
+except:
+    f.close()
